8ec0c7c3d60e47b7b026e6e55b6f7484fd06c0d3
cayetanogb22 <cayetanogb22@gmail.com>
2023-03-12 12:31:57 +0100

    Fin seccion 8

--- src/app/shared/breadcrumbs/breadcrumbs.component.ts
+++ src/app/shared/breadcrumbs/breadcrumbs.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
+import { ActivationEnd, Router } from '@angular/router';
+import { filter, map, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-breadcrumbs',
@@ -6,11 +8,29 @@ import { Component, OnInit } from '@angular/core';
   styles: [
   ]
 })
-export class BreadcrumbsComponent implements OnInit {
+export class BreadcrumbsComponent implements OnDestroy {
 
-  constructor() { }
+  titulo!: String;
+  tituloSubs$: Subscription = new Subscription();
 
-  ngOnInit(): void {
+  constructor(private router: Router) {
+    this.tituloSubs$ = this.getArgumentosRuta().subscribe(({ titulo }) => {
+      this.titulo = titulo;
+      document.title = `adminPro - ${titulo}`;
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.tituloSubs$.unsubscribe();
+  }
+
+  getArgumentosRuta() {
+    return this.router.events
+      .pipe(
+        filter((event): event is ActivationEnd => event instanceof ActivationEnd),
+        filter(event => event.snapshot.firstChild === null),
+        map((event: ActivationEnd) => event.snapshot.data),
+      );
   }
 
 }
